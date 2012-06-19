b7f49300b89405bd19dac97bade6b71e09a08f87
KayDebrah <debrah.kwadwo@gmail.com>
2012-06-19 15:44:21 +0000

    Python Lab Day 1

---
+++ Day1.py
@@ -0,0 +1,24 @@
+print "Hello AITI"
+print "Kwadwo Debrah"
+print "Hello, World"
+a=95
+#print c
+print "The value of a=", a
+print "The Value of 11/2=", 11/2
+print "The Value of 11.0/2=",11.0/2
+print "The Value of 3 to the power 27=", 3**27
+print "The value of (134533+6889980-84849)to the power 3=", (134533+6889980-84849)**3
+km=1000
+Distancemetres=7.9*km
+average=Distancemetres/45
+#print average
+print "The average time per meter=", average,"meters per minute"
+
+distance1=(4.7)**2
+distance2=(9.4)**2
+shortestdistance=distance1+distance2
+#print shortestdistance
+soln=shortestdistance**.5
+DistanceMiles=soln*0.621371
+
+print "Shortest Distance between point A and B=", DistanceMiles, "miles"
